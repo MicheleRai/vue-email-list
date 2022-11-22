808a4dd09e303f14bbe5314f694fcc40e66bb079
MicheleRai <rainonemichele1999@gmail.com>
2022-11-22 15:25:42 +0100

    first try

--- js/script.js
+++ js/script.js
@@ -5,9 +5,22 @@ Bonus
 Far comparire gli indirizzi email solamente quando sono stati tutti generati.*/
 
 
-var app = new Vue({
+new Vue({
     el: '#root',
     data: {
-
+      arrEmail: [],
+    },
+    methods: {
+      getArray() {
+        console.log(arrEmail);
+        for (let i = 0; i < 10; i++) { 
+          axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+          .then(axiosResponse => {
+            this.arrEmail.push(axiosResponse.data.response);
+          }) 
+        };
+        console.log(arrEmail);
+      }
     },
   });
+  
