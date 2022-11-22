9afa17d1a781d9fd7dfeb5be3980c4f1291efee3
MicheleRai <rainonemichele1999@gmail.com>
2022-11-22 16:58:31 +0100

    push check

--- js/script.js
+++ js/script.js
@@ -12,8 +12,7 @@ new Vue({
     },
     methods: {
       getArray() {
-        
-        console.log(arrEmail);
+        arrEmail = []
         for (let i = 0; i < 10; i++) { 
           axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
           .then(axiosResponse => {
